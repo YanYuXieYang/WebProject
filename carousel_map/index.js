375ef6eaeb4b1cd7ea82bdde7786bee31a07537e
yanyuxieyang <764613599@qq.com>
2021-01-17 00:27:10 +0800

    优化代码，规范函数注释和文件头部注释

--- carousel_map/index.js
+++ carousel_map/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: yanyuxieyang
+ * @Date: 2021-01-12 22:53:37
+ * @LastEditTime: 2021-01-17 00:26:00
+ * @LastEditors: Please set LastEditors
+ * @Description: carousel map
+ * @FilePath: \WebProject\carousel_map\index.js
+ */
 var timer = null,//定时器
   timerInv = 2000,//时间间隔
   index = 0,//图片索引
@@ -6,96 +14,105 @@ var timer = null,//定时器
 var box = document.getElementsByClassName('box')[0];
 var dot = document.getElementsByClassName('dot')[0];
 var li = dot.getElementsByTagName('li');
-var next = document.getElementsByClassName('next')[0];
 var last = document.getElementsByClassName('last')[0];
+var next = document.getElementsByClassName('next')[0];
 /**
-*轮播图片展示函数(最新，利用CSS3)，根据索引值到达相应地方
-*@param none
-*@return none
-*/
-function disPlay () {
-  if (index == img_count - 1) {
+ * @description: 轮播图片展示函数(最新，利用CSS3)，根据索引值到达相应地方
+ * @param {*}
+ * @return {*}
+ */
+function disPlay() {
+  if (index === img_count - 1) {
     moveTo(0);
     index = 0;
-  } else {
+  }
+  else {
     moveTo(++index);
   }
 }
-
 /**
-*上一张/下一章图片
-*Function lastPlay/nextPlay
-*@param none
-*@return none
-*/
-function lastPlay () {
-  if (index == 0) {
-    moveTo(img_count - 1);
+ * @description: 上一张图片
+ * @param {*}
+ * @return {*}
+ */
+function lastPlay() {
+  if (index === 0) {
     index = img_count - 1;
-  } else {
+    moveTo(img_count - 1);
+  }
+  else {
     moveTo(--index);
   }
 }
-function nextPlay () {
-  if (index == img_count - 1) {
-    moveTo(0);
+/**
+ * @description: 下一张图片
+ * @param {*}
+ * @return {*}
+ */
+function nextPlay() {
+  if (index === img_count - 1) {
     index = 0;
-  } else {
+    moveTo(0);
+  }
+  else {
     moveTo(++index);
   }
 }
 /**
-*绑定时间，包括小圆点、箭头的click事件
-*@function bindEvent
-*@param none
-*@return none
-*/
-function bindEvent () {
-  next.addEventListener('click', function () {
+ * @description: 设置被选中圆点的背景
+ * @param {*}
+ * @return {*}
+ */
+function setDotActBackground() {
+  for (var i = 0; i < img_count; i++) {
+    li[i].className = '';
+  }
+  li[index].className = 'act';
+}
+/**
+ * @description: 跳转到某张图片
+ * @param {*} location 索引值，具体位置存储在locatArr数组中
+ * @return {*}
+ */
+function moveTo(location) {
+  box.style.left = locatArr[location] + "px";
+  setDotActBackground();
+}
+/**
+ * @description: 绑定时间，包括小圆点、箭头的click事件
+ * @param {*}
+ * @return {*}
+ */
+function bindEvent() {
+  next.addEventListener('click', () => {
     clearInterval(timer);
     nextPlay();
-    timer = setInterval(disPlay, timerInv);
+    timer = window.setInterval(disPlay, timerInv);
   });
-  last.addEventListener('click', function () {
+  last.addEventListener('click', () => {
     clearInterval(timer);
     lastPlay();
-    timer = setInterval(disPlay, timerInv);
+    timer = window.setInterval(disPlay, timerInv);
   });
   for (var i = 0; i < img_count; i++) {
-    (function (j) {
-      li[j].addEventListener('click', function () {
+    ((j) => {
+      li[j].addEventListener('click', () => {
         clearInterval(timer);
         index = j;
         moveTo(index);
-        timer = setInterval(disPlay, timerInv);
-      })
-    }(i))//定义一个形参的匿名函数，并调用。实参为i
-
+        timer = window.setInterval(disPlay, timerInv);
+      });
+    })(i);//定义一个形参的匿名函数，并调用。实参为i
   }
 }
 /**
-*跳转到某张图片
-*@function moveTo
-*@param location 索引值，具体位置存储在locatArr数组中
-*@return none
-*/
-function moveTo (location) {
-  box.style.left = locatArr[location] + 'px';
-}
-function start () {
-
-  timer = setInterval(disPlay, timerInv);
-  var timerDot = setInterval(function () {
-    var dots = document.getElementsByClassName('dot')[0];
-    var singledot = dots.getElementsByTagName('li');
-    for (var i = 0; i < img_count; i++) {
-      singledot[i].className = '';
-    }
-    singledot[index].className = 'act';
-
-  }, 20)
+ * @description: 主函数入口
+ * @param {*}
+ * @return {*}
+ */
+function main() {
+  timer = window.setInterval(disPlay, timerInv);
   bindEvent();
 }
 
-start();
-
+main();
