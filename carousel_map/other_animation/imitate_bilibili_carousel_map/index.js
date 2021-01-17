9d1c4d02721684606c1fda4daa19f908314bc967
YanYuXieYang <361435262@qq.com>
2021-01-17 22:49:45 +0800

    nothing

--- carousel_map/other_animation/imitate_bilibili_carousel_map/index.js
+++ carousel_map/other_animation/imitate_bilibili_carousel_map/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: YanYuXieYang
  * @Date: 2021-01-17 21:32:25
- * @LastEditTime: 2021-01-17 22:48:51
+ * @LastEditTime: 2021-01-17 22:49:35
  * @LastEditors: Please set LastEditors
  * @Description: YanYuXieYang
  * @FilePath: \WebProject\carousel_map\other_animation\imitate_bilibili_carousel_map\index.js
